0516a499cd6a7bec233f217ccc680aa35a6a1e7b
AvSidwll <sidwellavuyilebatyi@gmail.con>
2023-07-07 14:54:05 +0200

    Updates

--- src/views/Projects-View.vue
+++ src/views/Projects-View.vue
@@ -1,25 +1,29 @@
 <template>
+  <div>
     <div v-if="myProjects" class="bg-dark row">
-      <myProjectComp id="proj" v-for="myProject of myProjects" :key="myProject.id" :myProject="myProject"/>  
+      <myProjectComp
+        id="proj"
+        v-for="myProject of myProjects"
+        :key="myProject.id"
+        :myProject="myProject"
+      />
     </div>
     <div v-else>Processing...</div>
+  </div>
 </template>
 <script>
-
-import myProjectComp from "@/components/Proj-Comp.vue"
+import myProjectComp from "@/components/Proj-Comp.vue";
 export default {
-    computed: {
-        myProjects() {
-            return this.$store.state.myProjects
-        }
+  computed: {
+    myProjects() {
+      return this.$store.state.myProjects;
     },
-    mounted() {
-        this.$store.dispatch("getmyProjects")
-    },
+  },
+  mounted() {
+    this.$store.dispatch("getmyProjects");
+  },
 
-    components: {myProjectComp}
-}
+  components: { myProjectComp },
+};
 </script>
-<style>
-    
-</style>
+<style></style>
